0f8e2fe252d79d5d89e1bcf7b6676b4d333ef95b
Taylor Thompson <n.taylor.thompson@gmail.com>
2015-07-14 09:54:12 -0400

    starting to edit devise user controller... might fuck up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714131733) do
+ActiveRecord::Schema.define(version: 20150714134004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 20150714131733) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "followed_things",        default: [],                 array: true
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
